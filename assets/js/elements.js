5fb58b84d1ed9334d5f31380057f77c1e7020669
Balthild Ires <stingzz.go@gmail.com>
2015-11-25 21:51:06 +0800

    Bootstrap界面

---
+++ assets/js/elements.js
@@ -0,0 +1,186 @@
+var elements = [
+	{ //There isn't a 0th element
+	},
+	{
+		"id": 1,
+		"cn": "氢",
+		"sym": "H",
+		"ram": 1,
+		"group": 1,
+		"pr": 1
+	},
+	{
+		"id": 2,
+		"cn": "氦",
+		"sym": "He",
+		"ram": 4,
+		"group": 18,
+		"pr": 1
+	},
+	{
+		"id": 3,
+		"cn": "锂",
+		"sym": "Li",
+		"ram": 7,
+		"group": 1,
+		"pr": 2
+	},
+	{
+		"id": 4,
+		"cn": "铍",
+		"sym": "Be",
+		"ram": 9,
+		"group": 2,
+		"pr": 2
+	},
+	{
+		"id": 5,
+		"cn": "硼",
+		"sym": "B",
+		"ram": 10,
+		"group": 13,
+		"pr": 2
+	},
+	{
+		"id": 6,
+		"cn": "碳",
+		"sym": "C",
+		"ram": 12,
+		"group": 14,
+		"pr": 2
+	},
+	{
+		"id": 7,
+		"cn": "氮",
+		"sym": "N",
+		"ram": 14,
+		"group": 15,
+		"pr": 2
+	},
+	{
+		"id": 8,
+		"cn": "氧",
+		"sym": "O",
+		"ram": 16,
+		"group": 16,
+		"pr": 2
+	},
+	{
+		"id": 9,
+		"cn": "氟",
+		"sym": "F",
+		"ram": 19,
+		"group": 17,
+		"pr": 2
+	},
+	{
+		"id": 10,
+		"cn": "氖",
+		"sym": "Ne",
+		"ram": 20,
+		"group": 18,
+		"pr": 2
+	},
+	{
+		"id": 11,
+		"cn": "钠",
+		"sym": "Na",
+		"ram": 23,
+		"group": 1,
+		"pr": 3
+	},
+	{
+		"id": 12,
+		"cn": "镁",
+		"sym": "Mg",
+		"ram": 24,
+		"group": 2,
+		"pr": 3
+	},
+	{
+		"id": 13,
+		"cn": "铝",
+		"sym": "Al",
+		"ram": 27,
+		"group": 13,
+		"pr": 3
+	},
+	{
+		"id": 14,
+		"cn": "硅",
+		"sym": "Si",
+		"ram": 28,
+		"group": 14,
+		"pr": 3
+	},
+	{
+		"id": 15,
+		"cn": "磷",
+		"sym": "P",
+		"ram": 31,
+		"group": 15,
+		"pr": 3
+	},
+	{
+		"id": 16,
+		"cn": "硫",
+		"sym": "S",
+		"ram": 32,
+		"group": 16,
+		"pr": 3
+	},
+	{
+		"id": 17,
+		"cn": "氯",
+		"sym": "Cl",
+		"ram": 35.5,
+		"group": 17,
+		"pr": 3
+	},
+	{
+		"id": 18,
+		"cn": "氩",
+		"sym": "Ar",
+		"ram": 40,
+		"group": 18,
+		"pr": 3
+	},
+	{
+		"id": 19,
+		"cn": "钾",
+		"sym": "K",
+		"ram": 39,
+		"group": 1,
+		"pr": 4
+	},
+	{
+		"id": 20,
+		"cn": "钙",
+		"sym": "Ca",
+		"ram": 40,
+		"group": 2,
+		"pr": 4
+	}
+];
+
+var groups = [
+	"undefined",
+	"I A",
+	"II A",
+	"III B",
+	"IV B",
+	"V B",
+	"VI B",
+	"VII B",
+	"VIII",
+	"VIII",
+	"VIII",
+	"I B",
+	"II B",
+	"III A",
+	"IV A",
+	"V A",
+	"VI A",
+	"VII A",
+	"0"
+]
