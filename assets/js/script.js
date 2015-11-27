1f730dd220f2d389a962cb6a86541cf1b59875c7
Balthild Ires <stingzz.go@gmail.com>
2015-11-27 22:10:26 +0800

    題目生成器

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,18 +15,21 @@ var elem = {
 			case 3:
 				picked = elements;
 				break;
+				
+			default:
+				return false;
 		}
 
 		var randArr = elem.genRandArr();
-		var questionElements = new Array(20);
-		for (i = 0; i < 20; i++) {
-			questionElements[i] = picked[randArr[i]];
+		var disordered = new Array(20);
+		for (var i = 0; i < 20; i++) {
+			disordered[i] = picked[randArr[i]];
 		}
-		alert(JSON.stringify(questionElements));
+		return disordered;
 	},
 	
 	genRandArr: function() {
-		// 这是网上抄来的打乱数组的代码，原理我也不知道 qwq
+		// 这是网上抄来的打乱数组的代码，原理我也不知道 quq
 		var arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 		var arr2 = [];
 		while(arr1.length > 0){
@@ -37,8 +40,99 @@ var elem = {
 	}
 };
 
+var game = {
+	start: function(mode) {
+		// 随机取出打乱的20个元素
+		var qElem = elem.pick(mode);
+		var questions = new Array(20);
+		for (var i = 0; i < qElem.length; i++) {
+			// 随机生成题目类型序号0-5，见templates
+			var rand = Math.floor(Math.random() * 6);
+			switch (rand) {
+				case 0:
+					questions[i] = [
+						templates[0][0].replace("{{id}}", qElem[i]["id"]),
+						qElem[i]["cn"]
+					];
+					break;
+				
+				case 1:
+					questions[i] = [
+						templates[1][0].replace("{{sym}}", qElem[i]["sym"]),
+						qElem[i]["cn"]
+					];
+					break;
+				
+				case 2:
+					questions[i] = [
+						templates[2][0].replace("{{group}}", groups[qElem[i]["gid"]]).replace("{{pr}}", qElem[i]["pr"]),
+						qElem[i]["cn"]
+					];
+					break;
+				
+				case 3:
+					questions[i] = [
+						templates[3][0].replace("{{sym}}", qElem[i]["sym"]),
+						groups[qElem[i]["gid"]] + "族" + qElem[i]["pr"] + "周期"
+					];
+					break;
+				
+				case 4:
+					questions[i] = [
+						templates[4][0].replace("{{cn}}", qElem[i]["cn"]),
+						qElem[i]["ram"]
+					];
+					break;
+				
+				case 5:
+					// 生成随机位置id，见pos
+					var posId = Math.floor(Math.random() * 4);
+					while (false) {
+						//
+					}
+					questions[i] = [
+						templates[5][0].replace("{{sym}}", qElem[i]["sym"]).replace("{{pos}}", pos[posId]),
+						elements[qElem["rel"][posId] - 1]["cn"]
+					];
+					break;
+				
+				default:
+					// code
+			}
+			$('#gamearea').append('<br>' + questions[i][0] + '<br>' + questions[i][1] + '<br>')
+		}
+		alert(JSON.stringify(qElem))
+	},
+	
+	correct: function() {
+		//
+	},
+	incorrect: function() {
+		//
+	},
+	
+	genBinArray: function() {
+		var binArray = new Array();
+		for (var i = 0; i < 21; i++) {
+			binArray[i] = [0, 1, 2, 3, 4, 5, 6, 7];
+		};
+	}
+};
+
+var score = [0, 0];
+
+var timer = {
+	start: function(timeout) {
+		
+	},
+	
+	reset: function() {
+		
+	}
+};
+
 jQuery(document).ready(function($) {
 	$('#mode1').click(function() {
-		elem.pick(1)
+		game.start(1);
 	})
 });
